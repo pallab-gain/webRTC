24da4537ed45e6dab4c2357e5af47d5e95e6be31
pallab-gain <pallab.gain@gmail.com>
2014-03-14 23:50:11 +0600

    implementing db features

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-var mongoose = require('mongoose');
+var User = require('./webrtc/model/user');
 
-var db = mongoose.connection;
+var userDB = new User();
 
-db.on('error', function(){
-    console.log('Unsuccessful connection');
+var user1 = userDB.add_user({name: 'Pallab1', phone: '01727013230'});
+var user2 = userDB.add_user({name: 'Pallab2', phone: '01727013230'});
+var user3 = userDB.add_user({name: 'Pallab3', phone: '01727013230'});
+
+userDB.save_user(user1, function (err, user, number_affect) {
+    //console.log(err, user, number_affect);
 });
-db.once('open', function () {
-    console.log('Connection successful');
+userDB.save_user(user2, function (err, user, number_affect) {
+    //console.log(err, user, number_affect);
 });
-mongoose.connect('mongodb://localhost/test');
-
-/*
- var kittenSchema = mongoose.Schema({
-    name: String
+userDB.save_user(user3, function (err, user, number_affect) {
+    //console.log(err, user, number_affect);
 });
 
- var kitten = mongoose.model('kitten', kittenSchema)*/
+userDB.find_user('01727013230', function (err, users) {
+    console.log(err, users);
+});
