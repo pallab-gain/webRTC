d285793a812f0e963d268ebf2152de67a01f81df
xerxes <xerxes@xerxes-pc.(none)>
2014-03-14 23:52:40 +0600

    implementing db features

--- webrtc/model/user.js
+++ webrtc/model/user.js
@@ -2,11 +2,40 @@
  * Created by xerxes on 3/14/14.
  */
 
-var user = function (options) {
-    var name = options.name || null;
-    var uid = options.uid;
+//var uuid = require('node-uuid');
+var mongo = require('mongoose');
+
+var db = mongo.connection;
+db.on('error', function () {
+    console.log('Unsuccessful connection');
+});
+db.once('open', function () {
+    console.log('Connection successful');
+});
+mongo.connect('mongodb://localhost/test');
+var userSchema = mongo.Schema({
+    name: String,
+    phone: String
+});
+var User = mongo.model('User', userSchema);
+
+var userController = function () {
     return {
-        name: name,
-        uid: uid
-    }
+        add_user: function (options) {
+            var name = options.name;
+            var phone = options.phone;
+            return  new User({name: name, phone: phone});
+        },
+        save_user: function (cur_user, callback) {
+            cur_user.save(function (err, cur_user, number_affect) {
+                callback(err, cur_user, number_affect);
+            });
+        },
+        find_user: function (phone, callback) {
+            User.find({phone: phone}).exec(function (err, users) {
+                callback(err, users);
+            })
+        }
+    };
 };
+module.exports = userController;
