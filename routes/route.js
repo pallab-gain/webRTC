0f04697f9227cb2386ce78c6a59ebc31fcc0c3f2
pallab-gain <pallab.gain@gmail.com>
2014-03-27 00:53:20 +0600

    learning....

---
+++ routes/route.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xerxes on 3/27/14.
+ */
+exports.login = function (req, res) {
+    res.sendfile('views/login.html');
+};
+
+exports.home = function (req, res) {
+    res.sendfile('views/home.html');
+}
