2cf3b7e6ed795e413dea798e8f28f0a75595ccf0
pallab-gain <pallab.gain@gmail.com>
2014-03-26 20:54:49 +0600

    lots of thing will going to change after this commit

--- webrtc/model/user.js
+++ webrtc/model/user.js
@@ -1,129 +1,18 @@
 /**
- * Created by xerxes on 3/14/14.
+ * Created by xerxes on 3/26/14.
  */
 
-//var uuid = require('node-uuid');
-var mongo = require('mongoose');
-
-var db = mongo.connection;
-db.on('error', function () {
-    console.log('Unsuccessful connection');
-});
-db.once('open', function () {
-    console.log('Connection successful');
-});
-mongo.connect('mongodb://localhost/test');
-var userSchema = mongo.Schema({
-    phone: String,
-    password: String,
-    status: Number,
-    buddy_phone: []
-});
-var User = mongo.model('User', userSchema);
-
-var userController = function () {
-    create_user = function (options, callback) {
-        var phone = options.phone;
-        var password = options.password;
-        var self = this;
-        self.is_exist(phone, function (err, exist) {
-            if (err) {
-                callback('server error', false);
-            } else {
-                if (exist) {
-                    callback('phone number already exist', null);
-                } else {
-                    callback(null, new User({password: password, phone: phone, status: 0}));
-                }
-            }
-        });
-    };
-    save_user = function (cur_user, callback) {
-        cur_user.save(function (err, cur_user, number_affect) {
-            callback(err, cur_user, number_affect);
-        });
-    };
-    find_user_by_token = function (token_id, callback) {
-        User.findOne({_id: token_id}).exec(function (err, user) {
-            if (err) {
-                callback('server error', null);
-            } else if (!user) {
-                callback('count not find user', null);
-            } else {
-                callback(null, user);
-            }
-        });
-    };
-    find_user = function (phone, callback) {
-        User.findOne({phone: phone}).exec(function (err, user) {
-            if (err) {
-                callback('server error', null);
-            } else if (!user) {
-                callback('could not find user', null);
-            }
-            else {
-                callback(null, user);
-            }
-        })
-    };
-    is_exist = function (phone, callback) {
-        User.findOne({phone: phone}).exec(function (err, user) {
-            if (err) {
-                callback('server error', null);
-            } else {
-                callback(null, user ? true : false);
-            }
-        });
-    };
-    is_valid_user = function (options, callback) {
-        var self = this;
-        self.find_user(options.phone, function (err, user) {
-            if (err) {
-                callback(err, null);
-            } else {
-                if (options.password === user.password) {
-                    callback(null, user);
-                } else {
-                    callback('invalid user', null);
-                }
-            }
-        })
-    };
-    add_buddy = function (options, callback) {
-        var self = this;
-        User.update({phone: options.phone},
-            {$push: {buddy_phone: options.buddy_phone}}, function (err, status) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, status);
-                }
-            }
-        )
-        ;
-    };
-    update_status = function (options, callback) {
-        var self = this;
-        User.update({phone: options.phone},
-            {$set: {status: options.status}}, function (err, status) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, status);
-                }
-            });
-    };
-
-    return {
-        create_user: create_user,
-        save_user: save_user,
-        find_user_by_token: find_user_by_token,
-        find_user: find_user,
-        is_exist: is_exist,
-        is_valid_user: is_valid_user,
-        add_buddy: add_buddy,
-        update_status: update_status
+var MySql = Bookshelf.initialize({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'webRTC',
+        charset: 'utf8'
     }
+});
 
-};
-module.exports = userController;
+var User = MySql.model.extend({
+    tableName: 'user'
+});
